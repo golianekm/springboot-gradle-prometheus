114753c8f94799f539ccb61cead67542cb62bf55
Marcin Golianek <Marcin.Golianek@asseco.pl>
2018-07-04 21:37:08 +0200

    tracing

--- src/main/groovy/golianekm/Rester.groovy
+++ src/main/groovy/golianekm/Rester.groovy
@@ -2,8 +2,10 @@ package golianekm
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -17,7 +19,10 @@ class Rester {
 	}
 
 	
-	@GetMapping("/people")
+	@Autowired
+	private io.opentracing.Tracer tracer;
+	
+	@RequestMapping("/people")
 	//@Timed(value = "people.all", longTask = false)
 	public List<String> listPeople() {
 		return ['ala','kot']
@@ -30,15 +35,16 @@ class Rester {
 	@Value('${SPRING_BOOT_PORT:N/A}')
 	private String service;
 
-	@GetMapping("/hostname")
+	@RequestMapping("/hostname")
 	//@Timed(value = "people.all", longTask = false)
 	public String hostname() {
 		log.info("Wywolano hostname")
-		try {
-			throw new RuntimeException("Byczek")
-		} catch ( e ) {
-			log.error( "Byczek:", e )
-		}
+		tracer.activeSpan().setBaggageItem("transaction", "hostname");
+		//try {
+		//	throw new RuntimeException("Byczek")
+		//} catch ( e ) {
+		//	log.error( "Byczek:", e )
+		//}
 		return "<html><body><h1>Hostname: ${hostname}</h1><h2>Service: ${service}</h2></body></html>"
 	}
 
--- src/main/groovy/golianekm/SpringbootPrometheusApplication.groovy
+++ src/main/groovy/golianekm/SpringbootPrometheusApplication.groovy
@@ -1,47 +1,78 @@
 package golianekm
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.session.SessionAutoConfiguration
 import org.springframework.context.annotation.Bean
+
+import com.uber.jaeger.Configuration
+import com.uber.jaeger.samplers.ProbabilisticSampler
 
 import io.micrometer.core.instrument.MeterRegistry
 import io.micrometer.core.instrument.binder.jvm.JvmThreadMetrics
 import io.micrometer.core.instrument.binder.system.FileDescriptorMetrics
 import io.micrometer.core.instrument.binder.system.UptimeMetrics
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry
 import io.micrometer.spring.autoconfigure.MeterRegistryCustomizer
+import io.opentracing.Tracer
+import io.opentracing.contrib.metrics.micrometer.MicrometerMetricsReporter
+import io.opentracing.tag.Tags
 
 @SpringBootApplication
-//@EnablePrometheusEndpoint
-//@EnableSpringBootMetricsCollector
-
 class SpringbootPrometheusApplication {
 
 	static void main(String[] args) {
 		SpringApplication.run SpringbootPrometheusApplication, args
 	}
-	
-	
+
+
 	@Bean
 	MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
-	  return { registry -> registry.config().commonTags("aplikacja", "spring-boot") }
+		return { MeterRegistry registry -> 
+			registry.config().commonTags("aplikacja", "spring-boot") 
+			//registry.config().
+			}
 	}
-	
+
 	@Bean
 	JvmThreadMetrics threadMetrics(){
 		return new JvmThreadMetrics();
 	}
-	
+
 	@Bean
 	FileDescriptorMetrics filesMetrics() {
 		return new FileDescriptorMetrics();
 	}
-	
+
 	@Bean
 	UptimeMetrics uptimeMetrics() {
 		return new UptimeMetrics();
 	}
+
+	@Autowired
+	MeterRegistry registry
 	
-	
-	
+	//@Bean
+	public io.opentracing.Tracer jaegerTracer() {
+		
+		//io.micrometer.core.instrument.Metrics.addRegistry(new SimpleMeterRegistry());
+		Tracer tracer = new Configuration("spring-boot", new Configuration.SamplerConfiguration(ProbabilisticSampler.TYPE, 1),
+			new Configuration.ReporterConfiguration(true,"192.168.1.200",6831,1000,1 ) )
+			.getTracer();
+		
+		// A reporter can then be created like this:
+		MicrometerMetricsReporter reporter = MicrometerMetricsReporter.newMetricsReporter()
+			.withName("tracing")
+			.withConstLabel("span.kind", Tags.SPAN_KIND_CLIENT)
+			.withRegistry(registry)
+			.withPercentiles(0.99)
+			.build();
+		
+		// Wrap the concrete Tracer, so that we can record the metrics about the reported spans
+		Tracer metricsTracer = io.opentracing.contrib.metrics.Metrics.decorate(tracer, reporter);
+		
+		return metricsTracer
+		
+		//return 	
+	}
 }
